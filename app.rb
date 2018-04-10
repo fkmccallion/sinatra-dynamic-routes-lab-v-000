c52c22c74bba0c8d445e344bf82510d77719eb85
Frank McCallion <fkmccallion@gmail.com>
2018-04-10 13:21:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -26,8 +26,27 @@ class App < Sinatra::Base
     @word3 = params[:word3]
     @word4 = params[:word4]
     @word5 = params[:word5]
-    #"#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}"
+
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+
+    case @operation
+      when "add"
+        @number1 + @number2
+      when "subtract"
+        @number1 - @number2
+      when "multiply"
+        @number1 * @number2
+      when "divide"
+        @number1 / @number2
+    end
+  end
+
   end
 
 end
