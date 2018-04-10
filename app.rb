366363aa8e93dda45ce331f448516c2e44dadc39
Frank McCallion <fkmccallion@gmail.com>
2018-04-10 13:23:02 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -37,15 +37,15 @@ class App < Sinatra::Base
 
     case @operation
       when "add"
-        @number1 + @number2
+        (@number1 + @number2).to_s
       when "subtract"
-        @number1 - @number2
+        (@number1 - @number2).to_s
       when "multiply"
-        @number1 * @number2
+        (@number1 * @number2).to_s
       when "divide"
-        @number1 / @number2
+        (@number1 / @number2).to_s
     end
-  
+
   end
 
 end
